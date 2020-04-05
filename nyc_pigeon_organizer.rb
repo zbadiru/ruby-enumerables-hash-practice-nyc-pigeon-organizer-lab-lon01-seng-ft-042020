179e7cc5abc51c7c0117aa70d4a46f6a9fcec9ad
Zakari Badiru <zbadiru@live.co.uk>
2020-04-05 12:10:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+new_hash = { }
+data.each do |key, value|
+  value.each do |new_value, names|
+    names.each do |name|
+      if !new_hash[name]
+        new_hash[name] = {}
+      end 
+      if !new_hash[name][key]
+        new_hash[name][key] = []
+      end
+      new_hash[name][key] << new_value.to_s
+    end
+  end
+end
+new_hash
 end
