80d9c6744defddd2b6750d2b01be154950364850
Zakari Badiru <zbadiru@live.co.uk>
2020-04-05 12:08:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ date.each do |key, value|
       if !new_hash[name]
         new_hash[name] = {}
       end 
-      if new_hash[name][key]
+      if !new_hash[name][key]
         new_hash[name][key] = []
       end
       new_hash[name][key] << new_value.to_s
